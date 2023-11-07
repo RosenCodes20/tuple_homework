b1cf97637fc5314edde692204a513b9d291aed40
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-11-07 20:06:36 +0200

    Add files via upload

---
+++ grades_of_the_class.py
@@ -0,0 +1,13 @@
+num = int(input())
+grades_dict = {}
+for i in range(num):
+    name, grade = input().split()
+    grade = float(grade)
+
+    if name not in grades_dict:
+        grades_dict[name] = []
+
+    grades_dict[name].append(grade)
+
+for names, grades in grades_dict.items():
+    print(f"{names} -> {' '.join([f'{grad:.2f}' for grad in grades])} (avg: {sum(grades) / len(grades):.2f})")
